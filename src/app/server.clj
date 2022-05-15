b9126049fc9bd9f3425c85a144dade83dcb8c155
Cintra Guilherme <cinguilherme@gmail.com>
2022-05-15 13:58:30 -0300

    attempt to expose routes endpoint

--- src/app/server.clj
+++ src/app/server.clj
@@ -14,12 +14,17 @@
   {:status 200 :body [{:name "x" :desc "XXX is cool" :active? false}
                       {:request request}]})
 
+(defn get-routes [_]
+  {:status 200 :body []})
+
+
 (defn make-routes [component-interceptor]
   (route/expand-routes
-    #{["/todo" :get get-todo :route-name :list-todo]
-      ["/rollouts" :get get-rollouts :route-name :list-rollouts]
-      ["/stuff" :get get-stuff :route-name :list-stuff]
-      ["/stuffx" :get [component-interceptor get-stuff] :route-name :list-stuffs]}))
+   #{["/todo" :get get-todo :route-name :list-todo]
+     ["/rollouts" :get get-rollouts :route-name :list-rollouts]
+     ["/stuff" :get get-stuff :route-name :list-stuff]
+     ["/stuffx" :get [component-interceptor get-stuff] :route-name :list-stuffs]
+     ["/routes" :get get-routes :route-name :list-routes]}))
 
 (def service-map
   {:env          :dev
