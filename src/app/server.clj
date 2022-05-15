f3519a7990f8a44e4d7392be4ea74aa1433ec6d2
cinguilherme <cinguilherme@gmail.com>
2022-05-15 14:07:54 -0300

    merge

--- src/app/server.clj
+++ src/app/server.clj
@@ -1,6 +1,7 @@
 (ns app.server
   (:require [io.pedestal.http.route :as route]
-            [io.pedestal.http :as http]))
+            [io.pedestal.http :as http]
+            [io.pedestal.http.body-params :as body-params]))
 
 
 (defn get-todo [_]
@@ -14,21 +15,27 @@
   {:status 200 :body [{:name "x" :desc "XXX is cool" :active? false}
                       {:request request}]})
 
-(defn get-routes [_]
-  {:status 200 :body []})
-
+(defn new-rollout
+  [{:keys [components edn-params] :as request}]
+  (println request)
+  (println edn-params)
+  (let [id (random-uuid)]
+    {:status 201 :body {:created :ok :rollout (assoc edn-params :id id)}})
+  )
 
 (defn make-routes [component-interceptor]
   (route/expand-routes
-   #{["/todo" :get get-todo :route-name :list-todo]
-     ["/rollouts" :get get-rollouts :route-name :list-rollouts]
-     ["/stuff" :get get-stuff :route-name :list-stuff]
-     ["/stuffx" :get [component-interceptor get-stuff] :route-name :list-stuffs]
-     ["/routes" :get get-routes :route-name :list-routes]}))
+    #{["/todo" :get get-todo :route-name :list-todo]
+      ["/rollouts" :get get-rollouts :route-name :list-rollouts]
+      ["/stuff" :get get-stuff :route-name :list-stuff]
+      ["/stuffx" :get [component-interceptor get-stuff] :route-name :list-stuffs]
+
+      ["/rollout" :post [component-interceptor (body-params/body-params) new-rollout] :route-name :new-rollout]
+      }))
 
 (def service-map
-  {:env          :dev
+  {:env         :dev
    ;::http/routes routes
-   ::http/type   :jetty
-   ::http/port   8081
-   ::http/join?  false})
+   ::http/type  :jetty
+   ::http/port  8081
+   ::http/join? false})
